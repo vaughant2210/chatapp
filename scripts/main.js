8f3f1915c2ac525c0bde2d2f4294e837f53fa6f3
Alex Martin <martin.alex.t+github@gmail.com>
2015-01-30 15:07:25 -0500

    Added message structure

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,12 @@
 
 // Object definitions
 var messageTemplate = _.template($('[data-template-name=message]').text());
-
+var messagePayload = {
+  username: '',
+  message: '',
+  created_at: Date.now(),
+  appName: "After12"
+};
 
 // Primary Data Functions (AJAX)
 var getMessages = function(callback) {
@@ -13,12 +18,12 @@ var getMessages = function(callback) {
     .done(function(d) { callback(d); });
 };
 
-
 // Helper Functions
 // getMessages(function(d) { console.log(d); });
 getMessages(function(data) {
   data.forEach(function(message) {
     if (!message.username) { return; }
+    console.log(message);
     $('.chatbox').append(messageTemplate(message));
   });
 });
