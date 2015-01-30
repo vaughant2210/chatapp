b2cbee34984417cd91e805e69766d9a42b96f53c
Alex Martin <martin.alex.t+github@gmail.com>
2015-01-30 13:09:20 -0500

    Scaffold JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,20 @@
+// Global Variables 
+//  -- try to keep this section SHORT! :)
 
+
+// Object definitions
+
+
+// Primary Data Functions (AJAX)
 var getMessages = function(callback) {
   $.ajax("http://tiny-pizza-server.herokuapp.com/collections/greenville-chats")
     .fail(function(err) { console.log(err); })
     .done(function(d) { callback(d); });
 };
 
+
+// Helper Functions
 getMessages(function(d) { console.log(d); });
+
+
+// Application Loop(s)
